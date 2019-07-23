a03faa04e336360d04c41eef473c5704ae2dd2b5
Stefan Minkov <sminkov@veritone.com>
2019-07-23 16:15:49 -0700

    add reverse_nested aggregation support

--- dist/elastic.js
+++ dist/elastic.js
@@ -1,6 +1,6 @@
-/*! elastic.js - v1.3.10 - 2018-04-10
- * https://github.com/fullscale/elastic.js
- * Copyright (c) 2018 FullScale Labs, LLC; Licensed MIT */
+/*! elastic.js - v1.3.11 - 2019-07-23
+ * https://github.com/veritone/elastic.js
+ * Copyright (c) 2019 veritone Labs, LLC; Licensed MIT */
 
 /**
  @namespace
@@ -4932,6 +4932,33 @@
 
     });
   };
+
+  /**
+    @class
+    <p>A special single bucket aggregation that enables aggregating reverse nested
+    documents.</p>
+
+    @name ejs.ReverseNestedAggregation
+    @ejs aggregation
+    @borrows ejs.AggregationMixin.aggregation as aggregation
+    @borrows ejs.AggregationMixin.agg as agg
+    @borrows ejs.AggregationMixin._type as _type
+    @borrows ejs.AggregationMixin.toJSON as toJSON
+
+    @desc
+    <p>A special single bucket aggregation that enables aggregating parent from nested
+    documents.</p>
+
+    @param {String} name The name which be used to refer to this aggregation.
+
+    */
+	ejs.ReverseNestedAggregation = function (name) {
+		var _common = ejs.AggregationMixin(name), 
+			agg = _common.toJSON();
+
+		agg[name].reverse_nested = {};
+		return _common;
+	};
 
   /**
     @class
